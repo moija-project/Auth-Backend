1de4b6abaabfa46064fd3a750165725de9db1e7a
SURI <snyeg28842@gmail.com>
2024-04-24 13:47:58 +0900

    feat: 앱~

---
+++ src/main/java/com/example/loginservice/dto/JwtToken.java
@@ -0,0 +1,13 @@
+package com.example.loginservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Builder
+@Data
+@AllArgsConstructor
+public class JwtToken {
+    private String grantType;
+    private String accessToken;
+}
